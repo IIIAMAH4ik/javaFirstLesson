0c853b13210eeae81f5637b0605828403642f74c
Tembulat <tembatokmakov43@gmail.com>
2026-02-03 13:49:40 +0300

    secondCommit

---
+++ src/main/java/firdLesson.java
@@ -0,0 +1,7 @@
+public class firdLesson {
+    public static void main(String[] args) {
+        String myStrings = "hello4_3"; //ссылочный тип данных
+        /*возможна конкотенация строк(сложение умножение и тд)
+        * строки можно обьединять с примитивными типами данных*/
+    }
+}
---
+++ src/main/java/secondLesson.java
@@ -0,0 +1,17 @@
+public class secondLesson {
+    public static void main(String[] args) {
+        int myInt;
+        myInt = 4;
+        var myVar = true; //musthave
+        double myDouble = 235.35;
+        float myFloat = 2362.4f;
+        char c = 'a';
+        boolean b = true;
+        short myShort = 3234;
+        long myLong = 235452334;
+        System.out.println(myInt);
+        System.out.print(myLong);
+
+
+    }
+}
