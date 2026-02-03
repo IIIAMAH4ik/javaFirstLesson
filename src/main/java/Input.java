01ff96bcb53d4c284c545ff07256941095fd2501
Tembulat <tembatokmakov43@gmail.com>
2026-02-03 16:34:11 +0300

    sfg

---
+++ src/main/java/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        var s = new Scanner(System.in);
+        System.out.println("введи");
+        var string = s.nextLine();
+        System.out.println(string);
+    }
+}
