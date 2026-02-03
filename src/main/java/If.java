19708f19d9fc2b106da432ab43a08af658f129cf
Tembulat <tembatokmakov43@gmail.com>
2026-02-03 14:12:25 +0300

    dgs

---
+++ src/main/java/If.java
@@ -0,0 +1,11 @@
+public class If {
+    public static void main(String[] args) {
+        if(5 == 4){
+            System.out.println("да");
+        } else if (4 == 4){
+            System.out.println(" да нэт");
+        } else {
+            System.out.println("нэт");
+        }
+    }
+}
